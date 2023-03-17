f15d0fce424ea4e9d989e8282d68f409fd05a4ae
Edoardo Gentile <edogen22122003@gmail.com>
2023-03-17 17:18:59 +0100

    Props-02 exercise

--- src/Component/Welcome.js
+++ src/Component/Welcome.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 export default class Welcome extends React.Component {
+    static defaultProps = {
+        name: 'Edoardo'
+    } 
+    
     render () {
         return (
             <p>"Welcome {this.props.name}!"</p>
