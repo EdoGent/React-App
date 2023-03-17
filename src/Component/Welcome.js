c43033b4de0830d6371d2865999ad7d6416eb86a
Edoardo Gentile <edogen22122003@gmail.com>
2023-03-17 17:30:17 +0100

    Props-03 exercise

--- src/Component/Welcome.js
+++ src/Component/Welcome.js
@@ -7,7 +7,10 @@ export default class Welcome extends React.Component {
     
     render () {
         return (
-            <p>"Welcome {this.props.name}!"</p>
+            <>
+                <p>"Welcome {this.props.name}!"</p>
+                <p>"Your age is {this.props.age}!"</p>
+            </>
         )
     }
 }
