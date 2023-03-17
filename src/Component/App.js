88056d3f184231f5aa0460a5fcbacadfed28fb36
Edoardo Gentile <edogen22122003@gmail.com>
2023-03-17 16:28:20 +0100

    Props-01 exercise

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hello from './Hello';
+import Welcome from './Welcome';
 
 class App extends React.Component {
     render () {
         return (
-            <div><Hello /></div>
+            <div>
+                <Hello />
+                <Welcome name = "Edoardo" />
+            </div>
         )
     }
 }
---
+++ src/Component/Welcome.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class Welcome extends React.Component {
+    render () {
+        return (
+            <p>"Welcome {this.props.name}!"</p>
+        )
+    }
+}
