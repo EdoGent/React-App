cfabd0f45fa248854c3ca54d63f66e3678ffacaa
Edoardo Gentile <edogen22122003@gmail.com>
2023-03-21 19:24:51 +0100

    State-01 exercise

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Counter from './Counter';
 import Hello from './Hello';
 
 class App extends React.Component {
     render () {
         return (
-            <div><Hello /></div>
+            <div>
+                <Hello />
+                <Counter />
+            </div>
         )
     }
 }
