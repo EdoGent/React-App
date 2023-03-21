aa87887fd1e388fba45b0211c9e6f89842a093b5
Edoardo Gentile <edogen22122003@gmail.com>
2023-03-21 16:34:47 +0100

    Props-05 exercise

--- src/Component/Welcome.js
+++ src/Component/Welcome.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Age from "./Age";
 
 export default class Welcome extends React.Component {
     static defaultProps = {
@@ -9,7 +10,7 @@ export default class Welcome extends React.Component {
         return (
             <>
                 <p>"Welcome {this.props.name}!"</p>
-                <p>"Your age is {this.props.age}!"</p>
+                <Age age='20' />
             </>
         )
     }
