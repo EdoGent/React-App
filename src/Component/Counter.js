3a5ea42504b18774490e6955e5f49377eeeadecf
Edoardo Gentile <edogen22122003@gmail.com>
2023-03-22 11:00:46 +0100

    Component-Lifecycle-01 exercise

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -2,19 +2,17 @@ import React from 'react';
 import CounterDispaly from './CounterDisplay';
 
 export default class Counter extends React.Component {
- state = {
-    count: this.props.initValue
- }
+    state = {
+        count: this.props.initValue
+    }
 
- constructor (props) {
-    super(props)
-    
-   setInterval(() => {
-       this.setState((state) => {
-           return {
-               count: state.count + this.props.incrementAmount
-           }
-       })
+ componentDidMount() {
+    setInterval(() => {
+        this.setState((state) => {
+            return {
+                count: state.count + this.props.incrementAmount
+            }
+        })
     }, this.props.interval)
  }
 
