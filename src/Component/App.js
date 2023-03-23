1ce53077ae1115ad42b4c08d327c4fa51e7922e7
Edoardo Gentile <edogen22122003@gmail.com>
2023-03-23 16:12:51 +0100

    Forms-05 exercise

--- src/Component/App.js
+++ src/Component/App.js
@@ -2,44 +2,15 @@ import React from 'react';
 import Hello from './Hello';
 import InteractiveWelcome from './InteractiveWelcome';
 import Welcome from './Welcome';
-import Login from './Login';
+import UncontrolledLogin from './UncontrolledLogin';
 
 class App extends React.Component {
-    state = {
-        username: '',
-        password: '',
-        remember: false,
-    }
-    
-    handleChangeInput = (event) => {
-        const value = event.target.value;
-        const name = event.target.name;
-        const type = event.target.type;
-        const checked = event.target.checked;
-
-        this.setState({
-            [name]: type === 'checkbox' ? checked : value
-        })
-    }
-
-    onLogin = (username, password) => {
-        console.log(username, password);
-    }
-
-    handleReset = () => {
-        this.setState({
-            username: '',
-            password: '',
-            remember: false
-        })
-    }
-
     render () {
         return (
             <div>
                 <Hello />
                 <InteractiveWelcome />
-                <Login onChangeInput={this.handleChangeInput} state={this.state} onLogin={this.onLogin} onReset={this.handleReset} />
+                <UncontrolledLogin />
             </div>
         )
     }
---
+++ src/Component/UncontrolledLogin.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export default class UncontrolledLogin extends React.Component { 
+    state = {
+        username: '',
+        password: '',
+        remember: false,
+    }
+
+    handleChangeInput = (event) => {
+        const value = event.target.value;
+        const name = event.target.name;
+        const type = event.target.type;
+        const checked = event.target.checked;
+
+        this.setState({
+            [name]: type === 'checkbox' ? checked : value
+        })
+    }
+
+    onLogin = (username, password) => {
+        console.log(username, password);
+    }
+
+    handleReset = () => {
+        this.setState({
+            username: '',
+            password: '',
+            remember: false
+        })
+    }
+
+    render () {
+       const {username, password, remember} = this.state;
+       const isDisabled = username === '' || password === '';
+        return (
+            <>
+                <input name='username' value={username} onChange={this.handleChangeInput}/>
+                <input name='password' value={password} type='password' onChange={this.handleChangeInput}/>
+                <input name='remember' type='checkbox' checked={remember} onChange={this.handleChangeInput}/>
+                <button disabled={isDisabled} onClick={() => this.onLogin(username, password)}>Login</button>
+                <button onClick={this.handleReset} >Reset</button>
+            </>
+        )
+    }
+}
