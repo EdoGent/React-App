4b937ad0dd5b88cee85046964ac13964445540eb
Edoardo Gentile <edogen22122003@gmail.com>
2023-03-25 16:30:19 +0100

    Component-Composition-01 exercise

--- src/Component/App.js
+++ src/Component/App.js
@@ -4,16 +4,17 @@ import InteractiveWelcome from './InteractiveWelcome';
 import Welcome from './Welcome';
 import UncontrolledLogin from './UncontrolledLogin';
 import ToDoList from './ToDoList';
+import Container from './Container';
 
 class App extends React.Component {
     render () {
         return (
-            <div>
+            <Container>
                 <Hello />
                 <InteractiveWelcome />
                 <UncontrolledLogin />
                 <ToDoList />
-            </div>
+            </Container>
         )
     }
 }
