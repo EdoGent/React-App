7852d8d3d9780e7fda3c8f5f1d279990152ae641
Edoardo Gentile <edogen22122003@gmail.com>
2023-03-28 15:47:58 +0200

    useEffect-01 exercise

--- src/Component/App.js
+++ src/Component/App.js
@@ -3,11 +3,14 @@ import ClickCounter from './ClickCounter';
 import Hello from './Hello';
 
 class App extends React.Component {
+    handleCounterChange = (clicks) => {
+        console.log(`The counter is ${clicks}`);
+    }
     render () {
         return (
             <div>
                 <Hello />
-                <ClickCounter />
+                <ClickCounter onCounterChange={this.handleCounterChange}/>
                 </div>
         )
     }
--- src/Component/ClickCounter.js
+++ src/Component/ClickCounter.js
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect , useState } from 'react';
 
-export default function ClickCounter() {
+export default function ClickCounter({ onCounterChange }) {
    const [clicks, setClicks] = useState(0)
+
+    useEffect(() => {
+        onCounterChange(clicks)
+    }, [clicks])
+
     return (
         <>
         <h1>Count : {clicks}</h1>
