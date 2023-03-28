85db20acb2b285769ea0c60e73df7b5732dda144
Edoardo Gentile <edogen22122003@gmail.com>
2023-03-28 17:50:26 +0200

    Custom-Hooks-01 exercise

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import { useEffect , useState } from 'react';
+import { useState } from 'react';
 
-export default function Counter () {
-    const [count, setCount] = useState(0)
-    
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCount(count => count + 1)
-        }, 1000);
+function useCounter (initialValue = 0) {
+    const [count, setCount] = useState(initialValue);
 
-        return () => clearInterval(interval);
-    }, [])   
+    const handleIncrement = () => {
+        setCount((count) => count + 1)
+    }
 
+    const handleDecrement = () => {
+        setCount((count) => count - 1)
+    }
+
+    const handleReset = () => {
+        setCount(initialValue)
+    }
+
+    return {
+        count: count,
+        onIncrement: handleIncrement,
+        onDecrement: handleDecrement,
+        onReset: handleReset
+    }
+}
+
+export default function Counter ({initialValue = 0}) {
+    const {count, onIncrement, onDecrement, onReset} = useCounter(initialValue)
     return (
-        <h1>Count : {count}</h1>
+        <div>
+            <h1>Count : {count}</h1>
+            <button onClick={onIncrement}>Increment</button>
+            <button onClick={onDecrement}>Decrement</button>
+            <button onClick={onReset}>Reset</button>
+        </div>
     )
 }
