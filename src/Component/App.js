db58886c5819ddfaad898b8617801d3d097d22fd
Edoardo Gentile <edogen22122003@gmail.com>
2023-03-30 00:28:35 +0200

    React-Router-04 exercise

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import ShowGithubUser from './ShowGithubUser';
 import Hello from './Hello'
+import Counter from './Counter'
 
 function App () {
         return (
             <div>
+                <div>
+                    <Link to='/'>Counter</Link> | <Link to='/users'>Hello message</Link> | <Link to='/users/username'>Show Github user</Link>
+                </div>
                 <Routes>
-                    <Route path='/' element={<Hello />} />
+                    <Route path='/' element={<Counter />} />
+                    <Route path='/users' element={<Hello />} />
                     <Route path='/users/:username' element={<ShowGithubUser />} />
                 </Routes>
             </div>
