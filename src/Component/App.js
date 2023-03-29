e28473793e2020b4fe964e3144c881540aef9a53
Edoardo Gentile <edogen22122003@gmail.com>
2023-03-29 19:29:23 +0200

    useMemo exercise

--- src/Component/App.js
+++ src/Component/App.js
@@ -3,16 +3,19 @@ import Counter from './Counter';
 import GithubUser from './GithubUser';
 import GithubUserList from './GithubUserList';
 import Hello from './Hello';
+import FilteredList from './FilteredList';
 
 function App () {
-        return (
-            <div>
-                <Hello />
-                <Counter />
-                <GithubUser username='EdoGent' />
-                <GithubUserList />
-            </div>
-        )
+    const object = [{name: 'John',id: 1,age: 25}, {name: 'Sara',id: 2,age: 12},{name: 'Mike', id: 3, age: 36}, {name: 'Kate',id: 4,age: 19}]
+    return (
+        <div>
+            <Hello />
+            <Counter />
+            <GithubUser username='EdoGent' />
+            <GithubUserList />
+            <FilteredList list={object} />
+        </div>
+    )
 }
 
 export default App;
---
+++ src/Component/FilteredList.js
@@ -0,0 +1,19 @@
+import React, { useMemo } from "react";
+
+function FilteredList({ list }) {
+  const filteredList = useMemo(() => list.filter(item => item.age > 18), [list]);
+
+  return (
+    <div>
+        <ul>
+            {filteredList.map(item => (
+            <li key={item.id}>
+                {item.name} - {item.age}
+            </li>
+            ))}
+        </ul>
+    </div>
+    );
+}
+
+export default FilteredList;
